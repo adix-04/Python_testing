e0fcbffcceeacf245bf47591af824998ba538996
Jithin Jayan Sreekala <jithin.sreekala@acsiatech.com>
2025-06-25 13:58:16 +0530

    python base code

---
+++ TTS_main.py
@@ -0,0 +1,14 @@
+import testui
+import pyttsx3
+
+
+def print_fun():
+    print("TTS file")
+
+# if __name__ == "__main__":
+
+    
+#     engine = pyttsx3.init()
+#     engine.setProperty("rate", 150)
+#     engine.say("Hey mini what is the time ")
+#     engine.runAndWait()
