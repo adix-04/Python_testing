a5ed73e4f1e0bf4bfa13ede6a6aa4fc106b1cd75
Jithin Jayan Sreekala <jithin.sreekala@acsiatech.com>
2025-06-26 15:56:29 +0530

    delected theard

--- start_dlt_theard.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-import time as t
-
-def start_DLT_with_prg():
-    process = subprocess.Popen(["C:/Users/jithin.sreekala/Downloads/release/release/dlt_viewer","-s","-p","C:/Users/jithin.sreekala/Downloads/release/release/file_DLT.dlp" ],stdout=subprocess.DEVNULL,stderr=subprocess.DEVNULL)
-
-if __name__ == "__main__":
-    start_DLT_with_prg()
