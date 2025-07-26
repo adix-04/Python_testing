76306b67240c6b4164166510176ecc2e492d0808
adix-04 <adinnvakumar22@gmail.com>
2025-07-26 21:41:59 +0530

    chore:arg parser function

--- src/TTS_main.py
+++ src/TTS_main.py
@@ -3,14 +3,12 @@ import logging
 import subprocess
 import os
 import pandas as pd
-from gtts import gTTS
 from pydub.playback import play
 import getpass
 import pyttsx3
-from Update_Excel import Update_Excel
 import threading
 import time as time 
-import re
+import argparse
 from datetime import datetime
 from Connect_DLT import Connet_DLT_class
 import utils
@@ -30,7 +28,6 @@ class Test_begin(object):
         self.report_excel_file = "output_from_test_run.xlsx"
         self.report_excel_file = self.outDIr + self.report_excel_file
         self.dlp = dlp_file
-        self.excel = Update_Excel()
         self.cache = f'C:/Users/{getpass.getuser()}/AppData/Local/dlt_viewer/cache'
         self.audioDir = 'audio'
 
@@ -174,100 +171,13 @@ class Test_begin(object):
             "--listen", "-i", "1", "--findfiles", "-f", "/data/local/tmp/file1", "-f", "/data/local/tmp/file2"
         ], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
-    def realtime_far_analyse(self,hitCount,stop_event):
-        print("real time far check")
-        log_file=f"{self.outDIr}/Target_log_WUW_FAR_Test.txt"
-        columns = ['WUW','Shown on HMI','Selected MIC','Correct MIC?','Highest ranked MIC',  
-                    'Ranking','Confidence','Standard Deviation','Conf.+StdDev','Avg(Conf.+StdDev)','GeoAvg(Conf, StdDev)',
-                    'Other detected MIC', 'Ranking2', 'Confidence2', 'Standard Deviation2','Conf.+StdDev2','Avg(Conf.+StdDev)2','GeoAvg(Conf, StdDev)2']
-        df = pd.DataFrame(columns=columns)
-        with open(log_file, "w", encoding='ISO-8859-1') as file:
-          print("opening logcat for device")
-        #   process = subprocess.Popen(["adb", "-s", self.mcuIp, "logcat"], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True, bufsize=1, encoding='ISO-8859-1')
-          process = subprocess.Popen(["adb" , "-s" ,"J7S8WCZTROEQ8L9D", "logcat"], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True, bufsize=1, encoding='ISO-8859-1')
-          temp_data = {}
-          while not stop_event.is_set():
-            line = process.stdout.readline()
-            file.writelines(f"{line}\n")
-            file.flush()
-            if "SpeechRecognizerEngineImpl" in line and "newState=BUSY" in line:
-            # if "GoogleRecognizer" in line and "listening" in line:
-                    print("speech reco is on boi")
-                    if temp_data:
-                        df.loc[len(df)] = [
-                            temp_data.get('WUW', ''),
-                            temp_data.get('Shown on HMI', False),
-                            #temp_data.get('Detected?', False),
-                            temp_data.get('Correct MIC?', False),
-                            temp_data.get('Highest ranked MIC', ''),
-                            temp_data.get('Ranking', ''),
-                            temp_data.get('Confidence', ''),
-                            temp_data.get('Standard Deviation', ''),
-                            temp_data.get('Conf.+StdDev', ''),
-                            temp_data.get('Avg(Conf.+StdDev)', ''),
-                            temp_data.get('GeoAvg(Conf, StdDev)', ''),
-                            temp_data.get('Other detected MIC', ''),
-                            temp_data.get('Ranking2', ''),
-                            temp_data.get('Confidence2', ''),
-                            temp_data.get('Standard Deviation2', ''),
-                            temp_data.get('Conf.+StdDev2', ''),
-                            temp_data.get('Avg(Conf.+StdDev)2', ''),
-                            temp_data.get('GeoAvg(Conf, StdDev)2', '')
-                        ]
-                        temp_data.clear()
-                    temp_data['WUW'] = "MINI WUW"
-                    temp_data['Shown on HMI'] = True
-                    logging.info("WUW detected!") 
-                    hitCount[0] += 1               
-            if "getHighestRankedResult() Highest" in line:
-                    temp_data['MIC'] = re.search("Highest: <Mic: (.*?),", line).group(1) if re.search("Highest: <Mic: (.*?),", line).group(1) else "No detection"
-                    temp_data['Ranking'] = re.search("ranking: (.*?)>", line).group(1) if re.search("ranking: (.*?)>", line).group(1) else "No detection"
-                    temp_data['Detected?'] = True
-                    if temp_data['MIC'] == self.mic:
-                        temp_data['Correct MIC?'] = True 
-                    temp_data['Highest ranked MIC'] = temp_data['MIC']
-                    logging.info(f"Mic: {temp_data['MIC']} detected, ranking is {temp_data['Ranking']}.")
+if __name__ == '__main__':
+   parser = argparse.ArgumentParser(description="run tts functions from with out UI")
+   parser.add_argument('--ip',type=str,required=True,help="ip address of the ECU")
+   parser.add_argument('--excel',type=str,required=True,help="excel containing utterances")
 
-            if "logRankings() 1 / 1: <Mic" in line:
-                    match = re.search("Mic: (.*?), ranking: \{ Total: (.*?), Conf: (.*?), StdDev: (.*?), Conf \+ StdDev: (.*?), Avg \(conf \+ stdDev\): (.*?), Geo avg \(conf, stdDev\): (.*?) \}",line)
-                    temp_data['Other detected MIC'] = match.group(1) if match.group(1) else "No detection"
-                    temp_data['Ranking2'] = match.group(2) if match.group(2) else "No detection"
-                    temp_data['Confidence2'] = match.group(3) if match.group(3) else "No detection"
-                    temp_data['Standard Deviation2'] = match.group(4) if match.group(4) else "No detection"
-                    temp_data['Conf.+StdDev2'] = match.group(5) if match.group(5) else "No detection"
-                    temp_data['Avg(Conf.+StdDev)2'] = match.group(6) if match.group(6) else "No detection"
-                    temp_data['GeoAvg(Conf, StdDev)2'] = match.group(7) if match.group(7) else "No detection"
-                    logging.info(f"Other MIC: {temp_data['Other detected MIC']}, Ranking: {temp_data['Ranking2']}, Confidence: {temp_data['Confidence2']}, Standard Deviation: {temp_data['Standard Deviation2']}, Conf.+StdDev: {temp_data['Conf.+StdDev2']}, Avg(Conf.+StdDev): {temp_data['Avg(Conf.+StdDev)2']}, GeoAvg(Conf, StdDev): {temp_data['GeoAvg(Conf, StdDev)2']}.")
-                
-            else:
-                if "SpeechRecognizerEngineImpl" in line and "newState=LISTENING" in line:
-                    logging.info("WUW detected!")
-                    hitCount[0] += 1
-        process.terminate()
-        print("gonna save the file")
-        df.to_excel(self.report_excel_file, index=False)
+   args = parser.parse_args()
 
-        # if temp_data:
-        #     print("temp data is true")
-        #     df.loc[len(df)] = [
-        #         temp_data.get('WUW', ''),
-        #         temp_data.get('Shown on HMI', False),
 
-        #         #temp_data.get('Detected?', False),
-        #         temp_data.get('Correct MIC?', False),
-        #         temp_data.get('Highest ranked MIC', ''),
-        #         temp_data.get('Ranking', ''),
-        #         temp_data.get('Confidence', ''),
-        #         temp_data.get('Standard Deviation', ''),
-        #         temp_data.get('Conf.+StdDev', ''),
-        #         temp_data.get('Avg(Conf.+StdDev)', ''),
-        #         temp_data.get('GeoAvg(Conf, StdDev)', ''),
-        #         temp_data.get('Other detected MIC', ''),
-        #         temp_data.get('Ranking2', ''),
-        #         temp_data.get('Confidence2', ''),
-        #         temp_data.get('Standard Deviation2', ''),
-        #         temp_data.get('Conf.+StdDev2', ''),
-        #         temp_data.get('Avg(Conf.+StdDev)2', ''),
-        #         temp_data.get('GeoAvg(Conf, StdDev)2', '')
-        #     ]
-          
+   print(args.ip)
+   print(args.excel)
